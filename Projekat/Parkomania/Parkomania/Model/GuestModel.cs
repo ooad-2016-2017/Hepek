905c6a316334aa9060601adc9ffaef5883647a3b
Admir Subasic <asubasic2@etf.unsa.ba>
2017-05-18 22:41:17 +0200

    Dodane ostale klase modela

---
+++ Projekat/Parkomania/Parkomania/Model/GuestModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parkomania.ViewModel
+{
+    class GuestModel
+    {
+        public List<Model.Guest> guests;
+        public GuestModel()
+        {
+            guests = new List<Model.Guest>();
+        }
+    }
+}
---
+++ Projekat/Parkomania/Parkomania/Model/System.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parkomania.ViewModel
+{
+    class System
+    {
+        public List<Model.Parking> parkings;
+        public List<Model.User> users;
+        public List<Model.Admin> admins;
+        public System()
+        {
+            parkings = new List<Model.Parking>();
+            users = new List<Model.User>();
+            admins = new List<Model.Admin>();
+        }
+    }
+}
