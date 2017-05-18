6edeb8ce042dfeebe580a72b548dddaf63da5379
Šubara Zlatko <zsubara1@etf.unsa.ba>
2017-05-18 00:30:50 -0700

    dodane neke klase

---
+++ Projekat/Parkomania/Parkomania/Model/Information.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parkomania.Model
+{
+    class Information
+    {
+        public DateTime cameAt;
+        public DateTime leftAt;
+        public Location Parking;
+        public DateTime Duration;
+        public Information(DateTime ca, DateTime lA, Location l, DateTime d)
+        {
+            cameAt = ca;
+            leftAt = lA;
+            Parking = l;
+            Duration = d;
+        }
+    }
+}
---
+++ Projekat/Parkomania/Parkomania/Model/ParkingModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parkomania.Model
+{
+    class ParkingModel
+    {
+        public double Length;
+        public double Width;
+        public List<Tuple<int, Location>> Locations;
+        public List<bool> Free;
+        public int brojParkingMjesta;
+        public ParkingModel(double l, double w)
+        {
+            Length = l;
+            Width = w;
+            Locations = new List<Tuple<int, Location>>();
+            Free = new List<bool>();
+            brojParkingMjesta = 0;
+        }
+        public void addLocation(Location l)
+        {
+            Locations.Add(new Tuple<int, Location>(brojParkingMjesta++, l));
+            Free.Add(true);
+        } 
+        public bool isFree(int i)
+        {
+            return Free[i];
+        }
+    }
+}
