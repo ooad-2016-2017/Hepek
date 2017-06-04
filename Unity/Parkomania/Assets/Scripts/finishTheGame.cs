e893d7d871b83807ebb22b4bf83d99ca8b4b3076
Šubara Zlatko <zsubara1@etf.unsa.ba>
2017-06-04 01:06:18 -0700

    igrica beta

---
+++ Unity/Parkomania/Assets/Scripts/finishTheGame.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityStandardAssets.Vehicles.Car;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class finishTheGame : MonoBehaviour {
+
+    public Canvas finishMenu;
+    public Button restartText;
+    public AudioSource asource;
+
+    void Start()
+    {
+        finishMenu = finishMenu.GetComponent<Canvas>();
+        restartText = restartText.GetComponent<Button>();
+        asource = asource.GetComponent<AudioSource>();
+        finishMenu.enabled = false;
+        asource.Pause();
+    }
+
+    // Update is called once per frame
+    void Update () {
+		if (transform.position.x > 87 && transform.position.x < 89 &&
+            transform.position.z > 62 && transform.position.z < 65)
+        {
+            Time.timeScale = 0;
+            asource.Play();
+            finishMenu.enabled = true;   
+        }
+	}
+    public void RestartLevel()
+    {
+        asource.Pause();
+        Time.timeScale = 1;
+        SceneManager.LoadScene("MainMenu");
+        finishMenu.enabled = false;
+    }
+}
---
+++ Unity/Parkomania/Assets/Scripts/hit.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class hit : MonoBehaviour {
+
+    public Canvas hitMenu;
+    public Button restartText;
+    public Button backText;
+
+    // Use this for initialization
+    void Start()
+    {
+        hitMenu = hitMenu.GetComponent<Canvas>();
+        restartText = restartText.GetComponent<Button>();
+        backText = backText.GetComponent<Button>();
+        hitMenu.enabled = false;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        
+    }
+    void OnCollisionEnter(Collision col)
+    {
+        hitMenu.enabled = true;
+        Time.timeScale = 0;
+    }
+    public void resrtart()
+    {
+        hitMenu.enabled = false;
+        Time.timeScale = 1;
+        SceneManager.LoadScene("first");
+    }
+    public void Back()
+    {
+        hitMenu.enabled = false;
+        Time.timeScale = 1;
+        SceneManager.LoadScene("MainMenu");
+    }
+}
